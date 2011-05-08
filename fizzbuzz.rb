fbab283c327f2c76c6991c3f1b07bb15faec8319
Stephen Aghaulor <saghaulor@gmail.com>
2011-05-08 19:42:10 -0400

    attempt at fizzbuzz

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+def fuzzbizz
+  n = 0
+  while n < 101
+    n += 1
+    if n % 15 == 0
+      puts n "is a fizzbuzz"
+    elsif n % 3 == 0
+      puts n "is a fizz"
+    elsif n % 5 == 0
+      puts n "is a buzz"
+    else
+      n += 1
+      print n
+    end
+  end
+end

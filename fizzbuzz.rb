ad78d82b0926dcd9d6ea0dd44a52c739708bdd91
Stephen Aghaulor <saghaulor@gmail.com>
2011-05-09 21:36:51 -0400

    fixed string manipulation of n

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,14 @@
 def fuzzbizz
   n = 0
+  n2 = n.to_s
   while n < 101
     n += 1
     if n % 15 == 0
-      puts n "is a fizzbuzz"
+      puts n2 + " is a fizzbuzz"
     elsif n % 3 == 0
-      puts n "is a fizz"
+      puts n2 + " is a fizz"
     elsif n % 5 == 0
-      puts n "is a buzz"
+      puts n2 + " is a buzz"
     else
       n += 1
       print n
